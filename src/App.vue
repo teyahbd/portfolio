0ba673eebc9c713208560d4a172d21875bd113cf
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-03-26 12:54:12 +0100

    feat: add basic scrollable pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>My Portfolio Site</div>
+  <div class="scroll-container">
+    <ScrollPage v-for="page in pages" :page="page" :key="page" />
+  </div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import ScrollPage from "./components/ScrollPage.vue";
+
+const pages: string[] = ["page 1", "page 2", "page 3"];
+</script>
+
+<style scoped>
+.scroll-container {
+  width: 100%;
+  height: 100%;
+  border: 1px solid red;
+}
+</style>
---
+++ src/components/ScrollPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-container">
+    <h1>{{ props.page }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps<{
+  page: string;
+}>();
+</script>
+
+<style scoped>
+.page-container {
+  height: 100vh;
+  border: 1px dashed blue;
+  width: 100%;
+}
+</style>
