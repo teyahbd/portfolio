a8f42e4226be5d9f0ecbd751d196998955f53809
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-04-13 18:02:50 +0100

    refactor: remove mobile from scroll containers

--- src/components/ScrollContainer.vue
+++ src/components/ScrollContainer.vue
@@ -3,17 +3,16 @@
     class="scroll-container"
     v-on:scroll="() => throttle(handleScroll, 500)"
   >
-    <ScrollPage
+    <WebScrollPage
       v-for="page in pages"
-      :page="page"
       :key="page"
-      id="ScrollPage"
-    />
+      :page="page"
+    ></WebScrollPage>
   </main>
 </template>
 
 <script setup lang="ts">
-import ScrollPage from "./ScrollPage.vue";
+import WebScrollPage from "./WebScrollPage.vue";
 import { usePageStore } from "../store/page";
 import { computed } from "vue";
 
@@ -29,15 +28,7 @@ const pageHeights = computed(() => [
   scrollPageHeight.value * 1.5,
 ]);
 
-const scrollPageWidth = computed(() => Math.floor(window.innerWidth));
-
-const pageWidths = computed(() => [
-  0,
-  scrollPageWidth.value / 2,
-  scrollPageWidth.value * 1.5,
-]);
-
-// turn into hook
+// turn into hook?
 let throttleTimer: boolean;
 
 const throttle = (callback: () => void, time: number) => {
@@ -52,13 +43,10 @@ const throttle = (callback: () => void, time: number) => {
 };
 
 const handleScroll = () => {
-  const isMobile = window.innerWidth <= 768;
+  const currentPosition =
+    document.getElementsByClassName("scroll-container")[0].scrollTop;
 
-  const currentPosition = isMobile
-    ? document.getElementsByClassName("scroll-container")[0].scrollLeft
-    : document.getElementsByClassName("scroll-container")[0].scrollTop;
-
-  const pageSizes = isMobile ? pageWidths.value : pageHeights.value;
+  const pageSizes = pageHeights.value;
 
   if (currentPosition < pageSizes[1]) {
     pageStore.updatePage(0);
@@ -74,18 +62,9 @@ const handleScroll = () => {
 .scroll-container {
   width: 100%;
   height: 100%;
-  /* border: 1px solid red; */
   display: flex;
   flex-direction: column;
   overflow-y: auto;
   scroll-snap-type: y mandatory;
-}
-
-@media only screen and (max-width: 768px) {
-  .scroll-container {
-    overflow-x: auto;
-    scroll-snap-type: x mandatory;
-    flex-direction: row;
-  }
 }
 </style>
--- src/components/WebScrollPage.vue
+++ src/components/WebScrollPage.vue
@@ -1,21 +1,28 @@
 <template>
   <section class="page-container">
-    <h1>{{ props.page }}</h1>
+    <ExamplePage1 v-if="props.page === 'page 1'" />
+    <ExamplePage2 v-if="props.page === 'page 2'" />
+    <ExamplePage3 v-if="props.page === 'page 3'" />
   </section>
 </template>
 
 <script setup lang="ts">
+import ExamplePage1 from "./ExamplePage1.vue";
+import ExamplePage2 from "./ExamplePage2.vue";
+import ExamplePage3 from "./ExamplePage3.vue";
+
 import { defineProps } from "vue";
 
 const props = defineProps<{
   page: string;
 }>();
+
+console.log(props.page);
 </script>
 
 <style scoped>
 .page-container {
   min-height: 88vh;
-  /* border: 1px dashed blue; */
   min-width: 100vw;
   scroll-snap-align: start;
   scroll-snap-stop: always;
